b8d4703d7fdfbb1c9cea0c3be4ed42ccf940391a
Marwan-G <ghabin2004@gmail.com>
2024-10-30 12:18:45 +0100

    add feature

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 const JobListing = ({job}) => {
+  const[showFulldescription,setFulldescription]=useState(false);
+  let description = job.description
+
+  if(!showFulldescription){
+    description = description.substring(0,49) +  '...';
+  }
   return (
             <div className="bg-white rounded-xl shadow-md relative">
               <div className="p-4">
@@ -10,13 +17,11 @@ const JobListing = ({job}) => {
                 </div>
       
                 <div className="mb-5">
-              {job.description}
+              {description}
                 </div>
-      
+                <button className='text-indigo-500 mb-5 hover:text-indigo-500'>{showFulldescription? 'Less': 'More'}</button>      
                 <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
-      
                 <div className="border border-gray-100 mb-5"></div>
-      
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                   <div className="text-orange-700 mb-3">
                     <i className="fa-solid fa-location-dot text-lg"></i>
